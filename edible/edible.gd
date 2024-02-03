7d534e707b3137021864cc737a5db097cd48b745
veprogames <75524847+veprogames@users.noreply.github.com>
2024-02-03 15:23:10 +0100

    edible spawn animation

--- edible/edible.gd
+++ edible/edible.gd
@@ -9,6 +9,13 @@ const EDIBLE_COLOR: Color = Color("#224fff")
 var edible: bool = false
 
 func _ready() -> void:
+	var previous_scale: Vector2 = sprite_2d.scale
+	sprite_2d.scale = Vector2.ZERO
+	var tween: Tween = create_tween()
+	tween.tween_property(sprite_2d, ^"scale", previous_scale, 1.0) \
+		.set_trans(Tween.TRANS_ELASTIC) \
+		.set_ease(Tween.EASE_OUT)
+	
 	sprite_2d.modulate.a *= randf_range(0.6, 0.8)
 
 func _on_area_entered(area: Area2D) -> void:
