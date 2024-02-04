44cb48c41d8e987210fb48b7b0f091b3001754d1
veprogames <75524847+veprogames@users.noreply.github.com>
2024-02-04 09:49:36 +0100

    CurrencyCounter interpolation (smooth increase)

--- ui/currency_counter.gd
+++ ui/currency_counter.gd
@@ -7,10 +7,22 @@ extends HBoxContainer
 @export var texture: Texture2D : set = _set_texture
 @export var value: float = 0.0 : set = _set_value
 
+@export_category("Interpolation")
+@export var interpolation_enabled: bool = false
+@export var interpolation_time: float = 0.4
+
+var displayed_value: float = 0.0
+
 var is_ready: bool = false
 
 func _ready() -> void:
 	is_ready = true
+	
+	if not interpolation_enabled:
+		process_mode = Node.PROCESS_MODE_DISABLED
+
+func _process(_delta: float) -> void:
+	label_value.text = "%.0f" % displayed_value
 
 func _set_texture(new_texture: Texture2D) -> void:
 	texture = new_texture
@@ -24,4 +36,11 @@ func _set_value(new_value: float) -> void:
 	
 	if not is_ready:
 		await ready
-	label_value.text = "%.0f" % value
+	
+	if interpolation_enabled:
+		var tween: Tween = create_tween()
+		tween.tween_property(self, ^"displayed_value", value, interpolation_time) \
+			.set_ease(Tween.EASE_OUT) \
+			.set_trans(Tween.TRANS_EXPO)
+	else:
+		label_value.text = "%.0f" % value
