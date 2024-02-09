73c0189330b27a6a294579ab121687ef9a7ce37c
veprogames <75524847+veprogames@users.noreply.github.com>
2024-02-09 11:04:32 +0100

    improve sound handling of trail collision areas

--- player/trail/collision/trail_collision_area.gd
+++ player/trail/collision/trail_collision_area.gd
@@ -7,6 +7,8 @@ extends Area2D
 @onready var timer: Timer = $Timer
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
+var sound_was_played: bool = false
+
 func _set_polygon(points: PackedVector2Array) -> void:
 	polygon = points
 	collision_polygon_2d.polygon = polygon
@@ -18,5 +20,10 @@ func set_remaining_time(seconds: float) -> void:
 	timer.wait_time = seconds
 	timer.start()
 
-func _ready() -> void:
-	audio_stream_player.play()
+func _on_area_entered(area: Area2D) -> void:
+	if sound_was_played:
+		return
+	
+	if area is Edible:
+		audio_stream_player.play()
+		sound_was_played = true
