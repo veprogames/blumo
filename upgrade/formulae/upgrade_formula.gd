695ff0d22db68a3e7dda4d0b68b384750d9f4e18
veprogames <75524847+veprogames@users.noreply.github.com>
2025-05-31 18:30:55 +0200

    Use Expression for upgrade formulas, change upgrade formulas

--- upgrade/formulae/upgrade_formula.gd
+++
@@ -1,5 +0,0 @@
-class_name UpgradeFormula
-extends Resource
-
-func get_value(_level: int) -> float:
-	return 0.0
--- upgrade/upgrade.gd
+++ upgrade/upgrade.gd
@@ -4,18 +4,47 @@ extends Resource
 signal leveled_up()
 
 @export var level: int = 0
-@export var price_formula: UpgradeFormula
-@export var effect_formula: UpgradeFormula
+
+@export var price_formula: String:
+	set(formula):
+		price_formula = formula
+		if price_expression.parse(price_formula, ["level"]) != OK:
+			push_warning(
+				"Parsing Formula for Upgrade %s failed: %s" % [
+					self,
+					price_expression.get_error_text()
+				]
+			)
+var price_expression: Expression = Expression.new()
+
+@export var effect_formula: String:
+	set(formula):
+		effect_formula = formula
+		if effect_expression.parse(effect_formula, ["level"]) != OK:
+			push_warning(
+				"Parsing Formula for Upgrade %s failed: %s" % [
+					self,
+					effect_expression.get_error_text()
+				]
+			)
+var effect_expression: Expression = Expression.new()
+
 @export var effect_display: EffectDisplayTemplate
 
 func get_effect_display() -> String:
 	return effect_display.get_value(get_current_effect())
 
 func get_current_price() -> float:
-	return price_formula.get_value(level)
+	var value: Variant = price_expression.execute([level])
+	if price_expression.has_execute_failed() or value == null:
+		return 0
+	return value
 
 func get_current_effect() -> float:
-	return effect_formula.get_value(level)
+	var value: Variant = effect_expression.execute([level])
+	if effect_expression.has_execute_failed() or value == null:
+		return 0
+	return value
 
 func can_buy_with(resource: float) -> bool:
 	return get_current_price() <= resource
