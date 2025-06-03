f3d5d3c720971dd7601ecd97bf4379b1b792f721
veprogames <75524847+veprogames@users.noreply.github.com>
2025-06-03 19:18:57 +0200

    upgrade popup menu

--- src/main_menu/main_menu.gd
+++ src/main_menu/main_menu.gd
@@ -1,19 +1,10 @@
 class_name MainMenu
 extends Node2D
 
-@onready var currency_counter: CurrencyCounter = $CanvasLayer/VBoxContainer/HBoxContainer/CurrencyCounter
-@onready var upgrade_button_trail: UpgradeButton = %UpgradeButtonTrail
-@onready var upgrade_button_value: UpgradeButton = %UpgradeButtonValue
+@onready var upgrade_menu: UpgradeMenu = $CanvasLayer/UpgradeMenu
 
 var LevelScene: PackedScene = preload("res://src/level/level.tscn")
 
-func _ready() -> void:
-	upgrade_button_trail.upgrade = Global.save.upgrade_trail_length
-	upgrade_button_value.upgrade = Global.save.upgrade_edible_value
-	
-	update_ui()
-	
-	Global.save.score_changed.connect(_on_score_changed)
 
 func _on_button_play_pressed() -> void:
 	SceneManager.goto_scene(LevelScene)
@@ -28,8 +19,6 @@ func _on_label_subtitle_meta_clicked(meta: Variant) -> void:
 	if url != null:
 		OS.shell_open(url)
 
-func _on_score_changed(_new_score: float) -> void:
-	update_ui()
 
-func update_ui() -> void:
-	currency_counter.value = Global.save.score
+func _on_button_upgrades_pressed() -> void:
+	upgrade_menu.show_menu()
---
+++ src/upgrade/upgrade_menu.gd
@@ -0,0 +1,43 @@
+class_name UpgradeMenu
+extends PanelContainer
+
+@onready var upgrade_button_trail: UpgradeButton = %UpgradeButtonTrail
+@onready var upgrade_button_value: UpgradeButton = %UpgradeButtonValue
+@onready var currency_counter: CurrencyCounter = %CurrencyCounter
+
+
+func _ready() -> void:
+	upgrade_button_trail.upgrade = Global.save.upgrade_trail_length
+	upgrade_button_value.upgrade = Global.save.upgrade_edible_value
+	
+	currency_counter.set_value_instant(Global.save.score)
+	Global.save.score_changed.connect(_on_global_score_changed)
+	
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
+	scale = Vector2.ZERO
+	show()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	var mouse_event: InputEventMouseButton = event as InputEventMouseButton
+	if mouse_event:
+		hide_menu()
+
+
+func show_menu() -> void:
+	mouse_filter = Control.MOUSE_FILTER_STOP
+	var tween: Tween = create_tween()
+	tween.tween_property(self, ^"scale", Vector2.ONE, 0.5) \
+		.set_trans(Tween.TRANS_ELASTIC) \
+		.set_ease(Tween.EASE_OUT)
+
+func hide_menu() -> void:
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
+	var tween: Tween = create_tween()
+	tween.tween_property(self, ^"scale", Vector2.ZERO, 0.3) \
+		.set_trans(Tween.TRANS_EXPO) \
+		.set_ease(Tween.EASE_OUT)
+
+
+func _on_global_score_changed(score: float) -> void:
+	currency_counter.value = Global.save.score
